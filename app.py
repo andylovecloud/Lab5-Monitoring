b8f0ee2597f0c2118cee22d10efaf6105bd439a0
AndyHuynh <andy.tranhuynh@gmail.com>
2025-04-15 13:49:21 +0300

    update new ports

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def metrics():
     return generate_latest(), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=56780)
